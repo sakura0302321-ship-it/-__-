703a6171b7f76c1f336066008708d22e17eda5e2
sakura0302321-ship-it <sakura0302321@gmail.com>
2026-03-06 14:40:38 +0900

    Add files via upload

---
+++ server.py
@@ -0,0 +1,11 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+class CORSHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type')
+        super().end_headers()
+
+print("Server started at http://localhost:8080")
+HTTPServer(('localhost', 8080), CORSHandler).serve_forever()
